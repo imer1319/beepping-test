4d42796349ef4b5ece4a9bb9203abc185cbb1093
Imer <imer1319nd@gmail.com>
2023-11-05 10:15:37 -0400

    migracines y modelos creados y configurados

---
+++ database/migrations/2023_11_05_141315_create_order_lines_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('order_id');
+            $table->bigInteger('product_id');
+            $table->integer('qty');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders');
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_lines');
+    }
+}
